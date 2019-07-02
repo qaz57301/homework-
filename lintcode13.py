6fb97b6ed13d61e2bb1d7a345405984bb5c1c6a6
qaz57301 <qaz57301@163.com>
2019-07-02 20:51:33 +0800

    Add files via upload

--- lintcode13.py
+++ lintcode13.py
@@ -1,3 +1,7 @@
+"""
+For a given source string and a target string, you should output the first index(from 0) of target string in source string.
+If target does not exist in source, just return -1.
+"""
 def contain_word(source,target):
     if source == "" and target=="":
         return 0
@@ -7,4 +11,4 @@ def contain_word(source,target):
             return source.index(i)
     return -1
 
-print(contain_word('abcd','ef'))
+print(contain_word('abcd','bc'))
